e6d36fe83ea4aab003e799aab32c9e79512358d9
Abdul-Akram <abdulakram921@gmail.com>
2024-08-24 14:30:00 +0530

    feat:Upload Files

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ToDo-React-UseReducer.git",
+  base: "/ToDo-React-UseReducer",
   plugins: [react()],
 });
